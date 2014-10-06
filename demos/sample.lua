f1ecac4683f152cb2e985c8f679ea8450bc1e250
Lucien Greathouse <me@lpghatguy.com>
2014-10-05 20:20:12 -0600

    Add demo loader, move messy.lua to demos/messy.lua

---
+++ demos/sample.lua
@@ -0,0 +1,6 @@
+--Sample demo
+--Use this as the template for writing your demos
+
+local Coeus = (...)
+
+--do stuff
--- main.lua
+++ main.lua
@@ -1,13 +1,36 @@
 --[[
 Demo Launcher
-This will launch demos in the future.
+Launches demos
 
-For now, check messy.lua for a (messy) sample app. It relies on the assets folder listed in the GitHub releases for this repository.
+Check demos/messy.lua for a (messy) sample app. It relies on the assets folder listed in the GitHub releases for this repository.
 ]]
 
 local ffi = require("ffi")
+local lfs = require("lfs")
 local config = require("config-debug")
 local Coeus = require("Coeus")
 Coeus:Initialize(config)
 
---todo: demo launcher
+print("Demos detected:")
+
+local idemo_list = {}
+for file in lfs.dir("demos") do
+	if (file:match("%.lua$")) then
+		local name = file:match("^(.-)%.lua$")
+		idemo_list[name] = true
+		print(name)
+	end
+end
+
+while (true) do
+	io.write("\nInput a demo name: ")
+	local input = io.read()
+
+	if (idemo_list[input]) then
+		local file = loadfile("demos/" .. input .. ".lua")
+		file(Coeus)
+		break
+	elseif (input == "quit") then
+		os.exit()
+	end
+end
